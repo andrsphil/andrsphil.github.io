8cd3188f66775514f4ce48b787eb601731abc354
Andreas Philippi <anphil@users.noreply.github.com>
2017-05-06 20:34:52 +0200

    Undo rainbow mode

--- assets/js/page.js
+++ assets/js/page.js
@@ -1,21 +1,5 @@
 (function()
 {
-  // Switch primary color every now and then
-  var body = document.getElementById('body');
-  var colors = ['dawn', 'grass', 'sky', 'sun'];
-  var current = -1;
-
-  window.setInterval(function()
-  {
-    var next = (current + 1) % colors.length;
-
-    body.classList.add(colors[next]);
-    body.classList.remove(colors[current]);
-
-    current = next;
-  }, 5000);
-
-
   // Email obfuscator script 2.1 by Tim Williams, University of Arizona
   // Random encryption key feature by Andrew Moulden, Site Engineering Ltd
   // This code is freeware provided these four comment lines remain intact
