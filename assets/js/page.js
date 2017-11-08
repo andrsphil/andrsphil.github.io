5f50cee9d316b966d8446f6fb3392489c36155cd
Andreas Philippi <anphil@users.noreply.github.com>
2017-11-08 09:47:18 +0100

    Update e-mail address

--- assets/js/page.js
+++ assets/js/page.js
@@ -5,8 +5,8 @@
   // This code is freeware provided these four comment lines remain intact
   // A wizard to generate this code is at http://www.jottings.com/obfuscator/
 
-  coded = "Ydppf@3-nY.6dI"
-  key = "EC9bLxHs1eKlAzZGRrMu5itdwpvQDSWNVgcXIa2nTFhqo740kUB3j68mYPfOyJ"
+  coded = "t-G8@7sYz77w.u3"
+  key = "bXyo9RY4jElGIdO62c7SFmKrPzhUuDQWxgAHeMakT85sfn0BVLZ3Ctw1iNpJvq"
   shift=coded.length
   link=""
   for (i=0; i<coded.length; i++) {
